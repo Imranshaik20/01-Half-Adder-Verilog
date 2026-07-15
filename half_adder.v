2aca7c0f56c7fa1bdd62d2e1244d7fa007fe58bc
Imranshaik20 <smdimran2008@gmail.com>
2026-07-16 00:10:08 +0530

    Initial commit - Half Adder using Verilog HDL

---
+++ half_adder.v
@@ -0,0 +1,19 @@
+//==================================================
+// Project : Half Adder
+// Author  : Imran Shaik
+//==================================================
+
+module half_adder(
+
+    input A,
+    input B,
+
+    output Sum,
+    output Carry
+
+);
+
+assign Sum   = A ^ B;
+assign Carry = A & B;
+
+endmodule
